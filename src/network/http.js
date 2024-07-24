24230046401545304726d2b15b1ac95ec8190f70
Seoyul Kim <zoeyul07@gmail.com>
2024-07-24 21:03:38 +0900

    refactor: 반복되는 로직 class 로 분리

---
+++ src/network/http.js
@@ -0,0 +1,27 @@
+export default class HttpClient {
+  constructor(baseURL) {
+    this.baseURL = baseURL;
+  }
+
+  async fetch(url, options) {
+    const res = await fetch(`${this.baseURL}${url}`, {
+      ...options,
+      headers: {
+        "Content-Type": "application/json",
+        ...options.headers,
+      },
+    });
+
+    try {
+      const data = await res.json();
+      if (res.status > 299 || res.status < 200) {
+        const message =
+          data && data.message ? data.message : "Something went wrong! 🤪";
+        throw new Error(message);
+      }
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
--- src/service/tweet.js
+++ src/service/tweet.js
@@ -1,57 +1,32 @@
 export default class TweetService {
-  constructor(baseURL) {
-    this.baseURL = baseURL;
+  constructor(http) {
+    this.http = http;
   }
-  async getTweets(username) {
-    const query = username ? `?username${username}` : "";
-    const response = await fetch(`${this.baseURL}/tweets${query}`, {
-      method: "GET",
-      headers: { "Content-Type": "application/json" },
-    });
 
-    const data = await response.json();
-    if (response.status !== 200) {
-      throw new Error(data.message);
-    }
-    return data;
+  async getTweets(username) {
+    const query = username ? `?username=${username}` : "";
+    return this.http.fetch(`/tweets${query}`, {
+      method: "GET",
+    });
   }
 
   async postTweet(text) {
-    const response = await fetch(`${this.baseURL}/tweets`, {
+    return this.http.fetch(`/tweets`, {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ text, username: "seoyul", name: "seoyul" }),
+      body: JSON.stringify({ text, username: "seoyul", name: "Seoyul" }),
     });
-
-    const data = await response.json();
-    if (response.status !== 201) {
-      throw new Error(data.message);
-    }
-    return data;
   }
 
   async deleteTweet(tweetId) {
-    const response = await fetch(`${this.baseURL}/tweets/${tweetId}`, {
+    return this.http.fetch(`/tweets/${tweetId}`, {
       method: "DELETE",
-      headers: { "Content-Type": "application/json" },
     });
-
-    if (response.status !== 204) {
-      throw new Error("not deleted");
-    }
   }
 
   async updateTweet(tweetId, text) {
-    const response = await fetch(`${this.baseURL}/tweets/${tweetId}`, {
+    return this.http.fetch(`/tweets/${tweetId}`, {
       method: "PUT",
-      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ text }),
     });
-
-    const data = await response.json();
-    if (response.status !== 201) {
-      throw new Error(data.message);
-    }
-    return data;
   }
 }
